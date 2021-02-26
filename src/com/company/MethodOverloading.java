0d3f02f46d78f9c8bb05827d8d96259c95e01abe
PushpRaj <uic.19bca1099@gmail.com>
2021-02-26 11:45:08 +0530

    MethodOverloading

---
+++ src/com/company/MethodOverloading.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class MethodOverloading {
+    public static void printn(int x){
+        System.out.println(x);
+    }
+    public static  void printn(double x){
+        System.out.println(x);
+    }
+    public static void printn(long x){
+        System.out.println(x);
+    }
+    public static void printn(float x){
+        System.out.println(x);
+    }
+
+}
